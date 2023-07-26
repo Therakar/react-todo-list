f33d3dc1dbba50478d965927a6ea77920d9ca5da
Therakar <matteo.pau200@gmail.com>
2023-07-26 13:16:54 +0200

    adds app background

--- src/components/HeaderComponents/Header.js
+++ src/components/HeaderComponents/Header.js
@@ -4,6 +4,7 @@ import FormAddTodo from "./FormAddTodo";
 export default function Header() {
   return (
     <div className="header">
+      <h1>TODO LIST</h1>
       <FormAddTodo />
     </div>
   );
