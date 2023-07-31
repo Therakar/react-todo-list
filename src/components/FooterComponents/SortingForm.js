851710a3628e6cfb84ff83289e4198b6f50dac8d
Therakar <matteo.pau200@gmail.com>
2023-07-31 13:29:40 +0200

    implements state of the add form

--- src/components/FooterComponents/SortingForm.js
+++ src/components/FooterComponents/SortingForm.js
@@ -5,8 +5,8 @@ export default function SortingForm() {
     <div className="sorting-container">
       <select className="shadow">
         <option value="input">Sort by input order</option>
-
-        <option value="packed">Sort by completion status</option>
+        <option value="description">Sort by name</option>
+        <option value="completed">Sort by completion status</option>
       </select>
       <button className="clear-btn">Clear List</button>
     </div>
--- src/components/HeaderComponents/FormAddTodo.js
+++ src/components/HeaderComponents/FormAddTodo.js
@@ -1,15 +1,35 @@
+import { useState } from "react";
 import "../../Style/HeaderStyle/FormAddTodo.css";
 import Button from "../GenericComponents/Button";
 
 export default function FormAddTodo() {
+  const [description, setDescription] = useState("");
+
+  // this function will handle the submit of the new to-do
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return; //clause guard (if there's not description return immediately)
+
+    //creation of the new item
+    const newItem = { description, completed: false, id: Date.now() };
+    console.log(newItem);
+
+    //reset
+    setDescription("");
+  }
+
   return (
-    <form className="add-form">
+    //when submit launch the fucntion handleSubmit
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>Let's add some to-dos 💪🏻</h3>
       <div>
         <input
           className="shadow"
           type="text"
-          placeholder="New activity..."
+          placeholder="New to-do..."
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
         ></input>
         <Button />
       </div>
